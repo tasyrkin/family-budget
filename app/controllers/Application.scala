537a0f34ea55b5a1fa500da22fad53b5c4483858
Timofey Asyrkin <tasyrkin@gmail.com>
2015-04-08 22:42:35 +0200

    Invoked main method of the views.html.main object and adjusted the routing

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,16 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
+import play.twirl.api.Html
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def main = Action {
+    Ok(views.html.main("Hello, world!")(Html("""<div id="1">This is a sample content</div>""")))
   }
 
 }
