894bd77e9ba02a07c11dc9b9886c7c6a75dbe1b9
Timofey Asyrkin <tasyrkin@gmail.com>
2015-04-09 22:34:01 +0200

    Started introducing domain model

---
+++ app/model/Currency.scala
@@ -0,0 +1,6 @@
+package model
+
+object Currency extends Enumeration{
+  type Currency = Value
+  val EUR, CAD, RUB, USD, JPY, GBP, AUD, CHF, MXN, CNY, NZD, SEK, HKD, NOK, SGD, TRY, KRW, ZAR, BRL, INR, DKK, ILS = Value                        
+}
---
+++ app/model/Expense.scala
@@ -0,0 +1,9 @@
+package model
+
+import org.joda.time.DateTime
+
+import Currency._
+
+case class Expense(currency : Currency, value: Int, created: DateTime) {
+
+}
