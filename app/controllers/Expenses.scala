d88a3384d0408705d25f657e313229e1a325aaad
Timofey Asyrkin <tasyrkin@gmail.com>
2015-04-11 09:02:59 +0200

    Introduced show controller method

--- app/controllers/Expenses.scala
+++ app/controllers/Expenses.scala
@@ -1,11 +1,24 @@
 package controllers
 
+import model.Expense
+import persistence.Db
 import play.api.mvc._
 
 object Expenses extends Controller {
   
   def list() = Action {
-    Ok(views.html.expenses())
+
+    val expenses = Db.query[Expense].fetch()
+
+    Ok(views.html.expenses(expenses))
   }
-  
+
+  def show(id: Long) = Action {
+    Ok(s"Showing expense $id")
+  }
+
+  def create() = Action {
+    Ok("Created")
+  }
+
 }
